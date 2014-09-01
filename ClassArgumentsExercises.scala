207f1b369c0a0ca4222d18292d649d84b9912bd5
iampeterbanjo <me@iampeterbanjo.com>
2014-09-01 05:36:27 +0100

    add class arguments exercises

--- ClassArgumentsExercises.scala
+++ ClassArgumentsExercises.scala
@@ -1,5 +1,6 @@
 import com.atomicscala.AtomicTest._
 
+// Exercise 1
 class Family(args:String*) {
 	def familySize(): Int = {
 		args.length
@@ -10,3 +11,20 @@ val family1 = new Family("Mom", "Dad", "Sally", "Dick")
 family1.familySize() is 4
 val family2 = new Family("Dad", "Mom", "Harry")
 family2.familySize() is 3
+
+// Exercise 2
+class FlexibleFamily(parent1:String, parent2:String, kids:String*) {
+
+	def familySize(): Int = {
+		kids.length + 2
+	}
+}
+
+val family3 = new FlexibleFamily("Mom", "Dad", "Sally", "Dick")
+family3.familySize() is 4
+val family4 = new FlexibleFamily("Dad", "Mom", "Harry")
+family4.familySize() is 3
+
+// Exercise 3
+val familyNoKids = new FlexibleFamily("Mom", "Dad")
+familyNoKids.familySize() is 2

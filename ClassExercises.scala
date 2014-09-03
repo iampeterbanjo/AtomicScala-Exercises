524bbbfa8b9774dbe9aa7ccddbec23814cc098cf
iampeterbanjo <me@iampeterbanjo.com>
2014-09-03 06:26:02 +0100

    add aux constructor for class exercise

--- ClassExercises.scala
+++ ClassExercises.scala
@@ -73,3 +73,32 @@ anotherST.minutes is 30
 val anotherST2 = new SimpleTimeDefault(10).subtract(new SimpleTimeDefault(9, 45))
 anotherST2.hours is 0
 anotherST2.minutes is 15
+
+println("\nExercise 6")
+class SimpleTimeAux(val hours: Int, val minutes: Int) {
+	
+	def this(hours: Int) {
+		this(hours: Int, 0)
+	}
+	
+	def totalTime(hrs: Int, mins: Int): Int = {
+		hrs * 60 + mins
+	}
+	
+	def subtract(simpleTime: SimpleTimeAux): SimpleTimeAux = {
+		val newTime = totalTime(hours, minutes) - 
+			totalTime(simpleTime.hours, simpleTime.minutes)
+		
+		new SimpleTimeAux(newTime/60, newTime % 60)
+	}
+}
+
+val auxT1 = new SimpleTimeAux(10, 5)
+val auxT2 = new SimpleTimeAux(6)
+val auxST = auxT1.subtract(auxT2)
+auxST.hours is 4
+auxST.minutes is 5
+val auxST2= new SimpleTimeAux(12).subtract(
+new SimpleTimeAux(9, 45))
+auxST2.hours is 2
+auxST2.minutes is 15
